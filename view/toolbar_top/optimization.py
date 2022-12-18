13d6b3b3748af22f3e2de0e141b33a2007e6080a
s-triar <34316314+s-triar@users.noreply.github.com>
2022-12-18 18:17:28 +0700

    using error opt > 1

--- view/toolbar_top/optimization.py
+++ view/toolbar_top/optimization.py
@@ -40,8 +40,8 @@ def click_btn_de_optimization(self, e):
     step_i = 1
     gen = []
     new_models=None
-    while(step_i<24):
-    # while(error_opt > 1):
+    # while(step_i<24):
+    while(error_opt > 1):
         self.btn_addmin_step_aligner.btn_increase.click()
         print("step_i",step_i)
         step_i+=1
